14eefa54725154bd0d0e00ec155e1c5d214fba5d
K <kimberlya159@nycstudents.net>
2026-01-18 19:25:02 -0500

    addition of js and appointment page

---
+++ script.js
@@ -0,0 +1,13 @@
+const setup = document.querySelector("#aptmt-setup");
+const service = document.querySelector("#service-type");
+
+
+const setup_Appointment = () => {
+    window.document.location = "setup.html";
+}
+const serviceSelection = () => {
+    
+}
+
+
+setup.addEventListener("click", setup_Appointment)
